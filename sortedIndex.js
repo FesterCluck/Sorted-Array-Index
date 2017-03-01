ffda44fc40dfa1db9d28913dfd899495492a0a41
Stephen Kraushaar <skraushaar@gmail.com>
2017-03-01 06:47:55 -0600

    Objects now supported, Filtering of entries
    Now supports Objects.
    
    A fourth parameter allowing for filtering of entries on the original object. Accepts a function which is passed the value of the entry. Should return a Boolean.

--- sortedIndex.js
+++ sortedIndex.js
@@ -1,28 +1,41 @@
-function createSortedIndex(toIndex, arrMethod, delegate) {
+function createSortedIndex(toIndex, arrMethod, delegate, filterFunc) {
 	var index = [];
 
 	arrMethod = arrMethod || "sort";
 
-	delegate = delegate || function() { return 0; };		
+	delegate = delegate || function() { return 0; };
 
-	toIndex.forEach(function(el, ix) {
+	var skipped = 0;
+	
+	function createGetter(item) {
+		var item = item;
+		return function() {
+			return toIndex[item];
+		};
+	}
+
+	Object.entries(toIndex).forEach(([key,val], ix) => {
+
+		if(filterFunc && !filterFunc(val)) {
+			skipped++;
+			return;
+		}
 		index.push({});
-		var cix = (index.length-1).toString();
+		var cix = (ix-skipped).toString();
 		Object.defineProperty(index, cix, {
 			enumerable : true,
 			configurable : true,
-			get : function() { return toIndex[ix]; },
+			get : createGetter(key),
 			set : function setterFunc(val) {
 				Object.defineProperty(index, cix, {
 					enumerable : true,
 					configurable : true,
-					get: function() { return toIndex[toIndex.indexOf(val)]; },
+					get: function() { return toIndex[Object.keys(toIndex)[Object.values(toIndex).indexOf(val)]]; },
 					set : Object.getOwnPropertyDescriptor(this, cix).set
 				});
-			} 
+			}
 		});
 	});
 
 	return index[arrMethod](delegate);
 }
-	
